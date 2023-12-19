62bace1d0f7c56cb698790f04760f231acfe4d4c
Melih Sunman <76569614+sunman54@users.noreply.github.com>
2023-12-19 10:43:34 +0300

    Update one_line_print.py

--- one_line_print.py
+++ one_line_print.py
@@ -1,11 +1,9 @@
 from time import sleep as wait
-import sys
 
 counter = 0
 while True:
     counter += 1
 
     print("\r Counter : " + str(counter), end='')
-    sys.stdout.flush()
     
     wait(0.8)
